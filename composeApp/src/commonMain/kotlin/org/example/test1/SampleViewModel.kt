0833c86872c716cf09dc6d295e7d819cb0ab5947
420locs <ninhtbm@gmail.com>
2025-03-19 20:12:09 +0700

    Add sample of Ktor client

--- composeApp/src/commonMain/kotlin/org/example/test1/SampleViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/example/test1/SampleViewModel.kt
@@ -5,25 +5,19 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import io.github.aakira.napier.Napier
 import kotlinx.coroutines.launch
-import org.example.test1.data.network.KtorClient
-import org.example.test1.data.network.SamplePlaceholderApi
 import org.example.test1.data.network.SamplePlaceholderRepository
 import org.example.test1.data.network.SampleTodo
 
 
 class SampleViewModel : ViewModel() {
     private val repository = SamplePlaceholderRepository()
-    var todo by mutableStateOf<List<SampleTodo>?>(null)
+    var todo by mutableStateOf<Result<List<SampleTodo>>?>(null)
         private set
-init {
-    test()
-}
+
     fun test() {
-        Napier.e("Nguuuuuuuuuuu")
         viewModelScope.launch {
-            todo = repository.getTodo()
+            todo = Result.success(repository.getTodo())
         }
     }
 }
